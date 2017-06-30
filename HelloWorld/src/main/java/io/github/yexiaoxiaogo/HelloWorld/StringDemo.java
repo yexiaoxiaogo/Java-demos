f54d40f3c147e473aa432fc5657a6f948752364c
yexiaoxiaogo <841079936@qq.com>
2017-06-30 17:57:55 +0800

    stringdemo

---
+++ HelloWorld/src/main/java/io/github/yexiaoxiaogo/HelloWorld/StringDemo.java
@@ -0,0 +1,18 @@
+package io.github.yexiaoxiaogo.HelloWorld;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		char[] helloArray = {'r','u','n','o','o','b'};
+//		String helloString = new String(helloArray);
+//		System.out.println(helloString);
+//		
+//		String site = "www.baidu.com";
+//		int len = site.length();
+//		System.out.println("网址长度为："+len);
+//		System.out.println("baidu的网址是"+site);
+
+	}
+
+}
