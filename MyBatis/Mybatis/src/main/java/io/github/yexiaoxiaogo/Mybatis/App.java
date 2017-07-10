b62cc44ca031adfcf4ec6977040433d1ae95c5ea
yexiaoxiaogo <841079936@qq.com>
2017-07-10 20:52:54 +0800

    mybatis java注释

--- MyBatis/Mybatis/src/main/java/io/github/yexiaoxiaogo/Mybatis/App.java
+++ MyBatis/Mybatis/src/main/java/io/github/yexiaoxiaogo/Mybatis/App.java
@@ -22,30 +22,98 @@ public class App
     public static void main( String[] args )
     {        
         MyDataSourceFactory mDataSourceFactory = new MyDataSourceFactory();
-//        mDataSourceFactory.setProperties(prop);
+
         DataSource dSource = mDataSourceFactory.getDataSource();
-//        mDataSourceFactory.showMsg();
-//        
-//        MyDataSourceFactory.showMsg();
-//        mDataSourceFactory.getDataSource();
-        
+
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
         Environment environment = new Environment("development", transactionFactory, dSource);
         Configuration configuration = new Configuration(environment);
         configuration.addMapper(MyMapper.class);
         
         sessionFactory = new SqlSessionFactoryBuilder().build(configuration);
-        try {
-        	SqlSession session = sessionFactory.openSession();
-			int numb = session.selectOne("getAllwebsites");
-			System.out.println(numb);
-			
-			Websites sWebsites = session.selectOne("geturlcountry", 2);
-			System.out.println(sWebsites);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-   
-       
+      
+        getAllwebsites();
+//        geturlcountry();
+//        insertWebsites();
+//        deleteWebsites();
+//        updataWebsites();
     }
+  //查询全部数据有几条
+    public static void getAllwebsites() {       
+    	try {
+    		SqlSession session = sessionFactory.openSession();
+    		
+    		int numb = session.selectOne("getAllwebsites");
+    		System.out.println(numb);
+   		
+    	} catch (Exception e) {
+    		// TODO: handle exception
+    		System.out.println(e);
+    	}
+	}
+  //通过id查询信息
+    public static void geturlcountry() {         
+    	try {
+    		SqlSession session = sessionFactory.openSession();
+  		
+    		Websites sWebsites = session.selectOne("geturlcountry", 2);
+    		System.out.println(sWebsites);
+    		
+    	} catch (Exception e) {
+    		// TODO: handle exception
+    		System.out.println(e);
+    	}
+	}
+	//增加一条信息
+    public static void insertWebsites() {
+    	try {
+    		SqlSession session = sessionFactory.openSession();
+
+    		Websites aWebsites =  new Websites();
+    		aWebsites.setId(9);
+    		aWebsites.setName("777");
+    		aWebsites.setUrl("777url");
+    		aWebsites.setAlexa(7);
+    		aWebsites.setCountry("cn");
+    		session.insert("insertWebsites",aWebsites );
+    		session.commit();
+    		Websites iWebsites = session.selectOne("geturlcountry", aWebsites.getId());
+    		System.out.println("增加的信息：" + iWebsites);
+
+    	} catch (Exception e) {
+    		// TODO: handle exception
+    		System.out.println(e);
+    	}
+	}
+	//删除一条信息
+    public static void deleteWebsites() {
+    	try {
+    		SqlSession session = sessionFactory.openSession();
+   	
+    		session.delete("deleteWebsites", 8);
+    		session.commit();
+    		int dWebsitesnumb = session.selectOne("getAllwebsites");
+    		System.out.println(dWebsitesnumb);
+
+    	} catch (Exception e) {
+    		// TODO: handle exception
+    		System.out.println(e);
+    	}
+	}
+	//修改一条信息
+    public static void updataWebsites() {
+    	try {
+    		SqlSession session = sessionFactory.openSession();
+
+    		Websites aWebsites =  new Websites();
+    		aWebsites.setId(1);
+    		aWebsites.setAlexa(2);
+    		int aaa=session.update("updataWebsites", aWebsites);
+    		session.commit();
+    		System.out.println("修改受影响的行数："+aaa);
+    	} catch (Exception e) {
+    		// TODO: handle exception
+    		System.out.println(e);
+    	}
+	}
 }
--- MyBatis/Mybatis/src/main/java/io/github/yexiaoxiaogo/Mybatis/MyMapper.java
+++ MyBatis/Mybatis/src/main/java/io/github/yexiaoxiaogo/Mybatis/MyMapper.java
@@ -1,6 +1,12 @@
 package io.github.yexiaoxiaogo.Mybatis;
 
+import java.util.jar.Attributes.Name;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.session.SqlSession;
 
 public interface MyMapper {
 	@Select("select count(*) from websites")
@@ -8,4 +14,15 @@ public interface MyMapper {
 
 	@Select("select url,country from websites where id = #{id}")
 	public Websites geturlcountry(int id);
+	
+	@Insert("insert into websites(id,name,url,alexa,country)values(#{id},#{name},#{url},#{alexa},#{country})")
+	public Websites  insertWebsites(Websites websites);
+	
+	@Delete("delete from websites where id=#{id}")
+	public Websites deleteWebsites(int id);
+	
+	@Update("update websites set alexa=#{alexa} where id=#{id}")
+	public Websites updataWebsites(Websites websites);
+	
 }
+
