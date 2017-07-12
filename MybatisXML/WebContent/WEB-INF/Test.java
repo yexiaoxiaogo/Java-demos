a44f515cac9a48e7f5ec34ac3d17cc500f8876b8
yexiaoxiaogo <841079936@qq.com>
2017-07-12 17:43:02 +0800

    update

---
+++ MybatisXML/WebContent/WEB-INF/Test.java
@@ -0,0 +1,41 @@
+package io.github.yexiaoxiaogo.MybatisXML;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Test {
+
+	private static SqlSessionFactory sqlSessionFactory;
+	private static Reader reader;
+
+	static {
+		try {
+			reader = Resources.getResourceAsReader("Configuration.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static SqlSessionFactory getSession() {
+		return sqlSessionFactory;
+	}
+
+	public static void main(String[] args) {
+
+		SqlSession session = sqlSessionFactory.openSession();
+		try {
+			Websites websites = (Websites) session
+					.selectOne("io.github.yexiaoxiaogo.MybatisXML.WebsitesrMapper.selectWebsitesByID", 1);
+			System.out.println(websites.getName());
+			System.out.println(websites.getUrl());
+		} finally {
+			session.close();
+		}
+	}
+
+}
